ab5f9fa02c8bbfb5652d6a2213b5a43a86307936
Farhan <farhansRepo@users.noreply.github.com>
2023-08-31 10:47:07 +0100

    Update romanNumerals and add tests

--- src/main/java/org/bowlingscore/App.java
+++ src/main/java/org/bowlingscore/App.java
@@ -5,12 +5,15 @@ package org.bowlingscore;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
-        BowlingScore bowlingScore = new BowlingScore();
+        RomanNumerals romanNumerals = new RomanNumerals();
 
-        bowlingScore.calculateScore("X X X X X X X X X X X X");
-
-        System.out.println(bowlingScore.calculateScore("X X X X X X X X X X X X"));
+        System.out.println(romanNumerals.decimalToRoman(100));
+//
+//        BowlingScore bowlingScore = new BowlingScore();
+//
+//        bowlingScore.calculateScore("X X X X X X X X X X X X");
+//
+//        System.out.println(bowlingScore.calculateScore("X X X X X X X X X X X X"));
     }
 }
--- src/test/java/org/bowlingscore/RomanNumeralsTest.java
+++ src/test/java/org/bowlingscore/RomanNumeralsTest.java
@@ -1,7 +1,63 @@
 package org.bowlingscore;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RomanNumeralsTest {
 
+    @Test
+    void checkDecimalToRomanCoverts100() {
+        //Arrange
+        final int INPUT_DECIMAL = 100;
+        final String EXPECTED = "C";
+        // Act
+        RomanNumerals romanNumerals = new RomanNumerals();
+        //Assert
+        assertEquals(EXPECTED,romanNumerals.decimalToRoman(INPUT_DECIMAL));
+    }
+
+    @Test
+    void checkDecimalToRomanCoverts3000() {
+        //Arrange
+        final int INPUT_DECIMAL = 3000;
+        final String EXPECTED = "MMM";
+        // Act
+        RomanNumerals romanNumerals = new RomanNumerals();
+        //Assert
+        assertEquals(EXPECTED,romanNumerals.decimalToRoman(INPUT_DECIMAL));
+    }
+
+    @Test
+    void checkDecimalToRomanCoverts444() {
+        //Arrange
+        final int INPUT_DECIMAL = 444;
+        final String EXPECTED = "CDXLIV";
+        // Act
+        RomanNumerals romanNumerals = new RomanNumerals();
+        //Assert
+        assertEquals(EXPECTED,romanNumerals.decimalToRoman(INPUT_DECIMAL));
+    }
+
+    @Test
+    void checkDecimalToRomanCoverts9() {
+        //Arrange
+        final int INPUT_DECIMAL = 9;
+        final String EXPECTED = "IX";
+        // Act
+        RomanNumerals romanNumerals = new RomanNumerals();
+        //Assert
+        assertEquals(EXPECTED,romanNumerals.decimalToRoman(INPUT_DECIMAL));
+    }
+
+    @Test
+    void checkDecimalToRomanCoverts1111() {
+        //Arrange
+        final int INPUT_DECIMAL = 1111;
+        final String EXPECTED = "MCXI";
+        // Act
+        RomanNumerals romanNumerals = new RomanNumerals();
+        //Assert
+        assertEquals(EXPECTED,romanNumerals.decimalToRoman(INPUT_DECIMAL));
+    }
 }
